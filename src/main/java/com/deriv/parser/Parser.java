51d916990ccee88e2cf2f89736767a580fcdeca7
geldra <norbert.valles@gmail.com>
2022-10-23 20:48:04 -0500

    funky java

--- src/main/java/com/deriv/parser/Parser.java
+++ src/main/java/com/deriv/parser/Parser.java
@@ -16,8 +16,8 @@ public class Parser {
 
   public Parser(String inputString) {
     System.out.print("I got this: \""+inputString + "\"");
-    this.inputString = inputString.replaceAll("\$","/");
-    System.out.print("Passing this down the line: \""+inputString.replaceAll("\$","/") + "\"");
+    this.inputString = inputString.replaceAll("\\$","/");
+    System.out.print("Passing this down the line: \""+inputString.replaceAll("\\$","/") + "\"");
   }
 
   /**
